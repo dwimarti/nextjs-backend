870866b305a783bcad91e13f26ee0dcbbf5a8c6f
Thor <thoriqnugroho3@gmail.com>
2022-01-13 13:03:57 +0700

    Update API, add websocket

--- pages/api/socketio.js
+++ pages/api/socketio.js
@@ -3,10 +3,34 @@ import siofu from "socketio-file-upload";
 import asterisk from "@libs/knexOrm/asterisk";
 import cors from "cors";
 import initMiddleware from "@libs/initMiddleware";
+import amxmlFetch from "@libs/amxml";
 
 const Cors = cors({
 	methods: ["GET", "POST"]
 })
+
+export const getQueueMember = async ({ queueName }) => {
+	try {
+		let listQueue = [];
+		const target = "http://"+ process.env.DB_HOST +":8088/amxml?action=queuestatus";
+		const request = await amxmlFetch(target, "client", "cleint123");
+		const response = request["ajax-response"]["response"];
+		for (let i = 1; i < response.length; i++) {
+			listQueue.push(response[i].generic[0]["$"]);
+		}
+		const kycQueue = listQueue.filter(
+			(e) =>
+				e.queue === queueName && e.event === "QueueMember" && e.status === "1" || e.status === "6"
+		);
+		if (kycQueue.length > 0) {
+			return { message: "agents available", data: kycQueue }
+		} else {
+			return { message: "no agents available", data: [] }
+		}
+	} catch (error) {
+		return { message: error, data: [] }
+	}
+} 
 
 async function IoHandler(req, res) {
 	await initMiddleware(req, res, Cors)
@@ -37,71 +61,52 @@ async function IoHandler(req, res) {
 				socket.emit("upload.error", e.message)
 			})
 
-			const safeJoin = (currentId) => {
-				socket.leave(previousId);
-				socket.join(currentId, () =>
-					console.log(`Socket ${socket.id} joined room ${currentId}`)
-				);
-				previousId = currentId;
-			};
+			socket.on("start", () => {
+				socket.emit("start", "halo ler")
+			})
 
-			socket.on("joinRoom", (idRoom) => {
-				safeJoin(idRoom);
-				const finder = chat.find((e) => e.id === idRoom);
-				if (finder) {
-					socket.emit("joinRoom", finder);
-				} else {
-					chat.push({ id: idRoom, content: [] });
-					socket.emit(
-						"joinRoom",
-						chat.find((e) => e.id === idRoom)
-					);
-				}
-			});
-
-			socket.on("sendChat", (data) => {
-				safeJoin(data.id);
-				const finder = chat.find((e) => e.id === data.id);
-				finder.content.push(data.content);
-				io.emit("allChat", chat);
-				socket.emit("chatBroadcast", finder);
-			});
-
-			socket.on("agentlist", function () {
-				setInterval(() => {
-					asterisk
-						.select("username", "status", "websocket")
-						.from("users")
-						.where({ role: "1" })
-						.orderBy("username")
-						.then((data) => {
-							socket.emit("agentlist", data);
-						})
-						.catch((err) => {
-							socket.emit("agentlist", err.message);
-						});
+			socket.on("queue", () => {
+				let interval;
+				interval = setInterval(async () => {
+					const result = await getQueueMember({queueName: "kyc"})
+					socket.emit("queue", result)
 				}, 1000);
-			});
 
-			socket.on("login", (data) => {
-				safeJoin(data.id);
-				console.log(data);
-				socket.emit("login", data);
-			});
+				setTimeout(async () => {
+					clearInterval(interval)
+					const result = await getQueueMember({queueName: "kyc"})
+					if (result.data.length <= 0) {
+						socket.emit("queue", { message: "no agents available, please try again next time"})
+					} else {
+						socket.emit("queue", { message: "agent available"})
+					}
+				}, 29000);
+			})
+
+			// socket.on("agentlist", function () {
+			// 	setInterval(() => {
+			// 		asterisk
+			// 			.select("username", "status", "websocket")
+			// 			.from("users")
+			// 			.where({ role: "1" })
+			// 			.orderBy("username")
+			// 			.then((data) => {
+			// 				socket.emit("agentlist", data);
+			// 			})
+			// 			.catch((err) => {
+			// 				socket.emit("agentlist", err.message);
+			// 			});
+			// 	}, 1000);
+			// });
 
 			const session = req.body.src;
 			socket.broadcast.emit("a user connected ", req.body.src);
 			socket.on(session, (data) => {
 				socket.emit(session, `Hallo ${session} u send ${data}`);
 			});
-			socket.on("hello", (msg) => {
-				setInterval(() => {
-					socket.emit("hello", `mate, now ${new Date().toLocaleTimeString()}`);
-				}, 1000);
-			});
-			socket.on("chat", (data) => {
-				socket.emit("chat", data);
-			});
+			socket.on('disconnect', function() {
+        console.log('socket disconnect');
+    });
 		});
 
 		res.socket.server.io = io;
@@ -114,7 +119,7 @@ async function IoHandler(req, res) {
 export const config = {
 	api: {
 		bodyParser: {
-			sizeLimit: "1mb",
+			sizeLimit: "100mb",
 		},
 	},
 };
