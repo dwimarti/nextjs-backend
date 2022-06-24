b73f266f3e65358dbbfe6b1ae596da812894e768
Aldesfi Arifin <alldes182@gmail.com>
2022-06-24 11:22:01 +0700

    add authorization EXPIRED TIME
    add authorization EXPIRED TIME

--- libs/authorization.js
+++ libs/authorization.js
@@ -2,10 +2,31 @@ import jwt from 'jsonwebtoken';
 
 export default function authorization(jwtToken,res) {
   try {
-    const token = jwtToken.split(' ')[2];
-    jwt.verify(token, process.env.JWT_KEY);
+	console.log(jwtToken);
+    const token = jwtToken.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+   
+     //token-expired-validation
+      const currentDate = new Date();
+      const timestamp = currentDate.getTime();
+      const timestampGap = timestamp-decodedToken.timeStamp;
+
+      //debug
+      console.log(decodedToken.timeStamp);
+      console.log(timestamp-decodedToken.timeStamp);
+
+      if(process.env.JWT_EXPIRED<timestampGap){
+        console.log("TOKEN EXPIRED");
+        res.status(403).json({
+          status: 0,
+          message: "JWT TOKEN EXPIRED",
+        });
+        return true;
+      }
+      
     return false;
   } catch (e) {
+    console.log(e);
     res.status(403).json({
       status: 0,
       message: "JWT TOKEN INVALID",
